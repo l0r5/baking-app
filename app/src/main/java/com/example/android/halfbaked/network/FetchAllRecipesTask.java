0e2a7b754aef4aac775de26953aa92f97f420902
Lars Braitinger <lars.braitinger@gmail.com>
2017-08-19 11:35:29 +0200

    create AsyncTaskLoader to fetch the recipe data from the API

---
+++ app/src/main/java/com/example/android/halfbaked/network/FetchAllRecipesTask.java
@@ -0,0 +1,84 @@
+package com.example.android.halfbaked.network;
+
+
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.android.halfbaked.models.Recipe;
+
+import java.net.URL;
+
+
+public class FetchAllRecipesTask implements LoaderManager.LoaderCallbacks<Recipe[]> {
+
+    public interface FetchAllRecipesCallback {
+
+        void onFetchAllRecipesTaskCompleted(Recipe[] recipes);
+    }
+
+    private static final String TAG = FetchAllRecipesTask.class.getSimpleName();
+    private FetchAllRecipesCallback mCallback;
+    private Context mContext;
+
+    public FetchAllRecipesTask(FetchAllRecipesCallback callback, Context context) {
+        this.mCallback = callback;
+        this.mContext = context;
+    }
+
+    @Override
+    public Loader<Recipe[]> onCreateLoader(int i, Bundle bundle) {
+        return new AsyncTaskLoader<Recipe[]>(mContext) {
+
+            Recipe[] recipeData = null;
+
+            @Override
+            protected void onStartLoading() {
+                if (recipeData != null) {
+                    deliverResult(recipeData);
+                } else {
+                    forceLoad();
+                }
+            }
+
+            @Override
+            public Recipe[] loadInBackground() {
+                URL recipeRequestUrl = NetworkUtils.buildRecipeCollectionUrl(mContext);
+
+                try {
+                    String jsonRecipeResponse = NetworkUtils.getResponseFromHttpUrl(recipeRequestUrl);
+
+                    // TODO DataFormUtil json -> objects
+
+                    Log.i(TAG, "Incoming Recipe JSON:");
+                    Log.i(TAG, jsonRecipeResponse);
+
+                    return null;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            public void deliverResult(Recipe[] data) {
+                recipeData = data;
+                super.deliverResult(recipeData);
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Recipe[]> loader, Recipe[] data) {
+        mCallback.onFetchAllRecipesTaskCompleted(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Recipe[]> loader) {
+
+    }
+}
